b9725d0514f7eef1219e3eb5b66cbf80341b4dc2
AlexisEGM <alexisgonzalez.itsu@gmail.com>
2026-05-29 01:24:35 -0400

    chore: allow tracking of dist folder in .gitignore

--- asignacion-03/vite.config.js
+++ asignacion-03/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Asegura que las rutas de los assets sean relativas para GitHub Pages
+  base: '/PortafolioPersonal/asignacion-03/dist/', //
 })
